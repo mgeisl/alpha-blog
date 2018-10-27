60544252b115318a37064179d4f76cdc415d0b58
mgeisl <maxwell.m.geisler@gmail.com>
2018-10-27 05:29:56 +0000

    another heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
+  ##test to get heroku to deploy
+  config.assets.initialize_on_precompile = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
